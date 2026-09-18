9878fbfa0f373693ccc42a6ac2f723edd425ea34
kons2003 <96938589+kons2003@users.noreply.github.com>
2026-09-18 12:53:34 +0900

    Fix cookie header concatenation in useAxios function

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -5,5 +5,5 @@ type AXIOS_METHOD = 'POST' | 'GET';
 export async function useAxios(url:string, method: AXIOS_METHOD, data?: any, query?: string[]) {
     if (data !== undefined) return axios({ url: url, method: method, data: data })
 
-    return axios({ url: url, method: method, headers: {Cookie: query?.concat('; ')} })
+    return axios({ url: url, method: method, headers: {Cookie: query?.join('; ')} })
 }
